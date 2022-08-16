88d75410d0933b4ae4a618139a08970d09a0f2fd
Vitor Duarte <vitorduarte.vds7@gmail.com>
2022-08-16 16:48:16 -0300

    first

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require("express");
+
+const app = express();
+
+// app.get(express.json());
+
+app.get("/hello", (req, res) => {
+    res.send("Ha");
+});
+
+app.listen(3000, () => {
+    console.log("ta rodando");
+});
